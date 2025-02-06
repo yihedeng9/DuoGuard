a863b15f30ee79110aacbd82f15fbd3c77b91eed
yihedeng9 <yihedeng@cs.ucla.edu>
2025-02-05 22:17:11 -0800

    update eval script

---
+++ scripts/eval.sh
@@ -0,0 +1,14 @@
+python ./evaluation/evaluate_duoguard.py --language En
+python ./evaluation/evaluate_duoguard.py --language Fr
+python ./evaluation/evaluate_duoguard.py --language Es
+python ./evaluation/evaluate_duoguard.py --language De
+
+python ./evaluation/evaluate_shieldgemma.py --language En
+python ./evaluation/evaluate_shieldgemma.py --language Fr
+python ./evaluation/evaluate_shieldgemma.py --language Es
+python ./evaluation/evaluate_shieldgemma.py --language De
+
+python ./evaluation/evaluate_llamaguard.py --language En
+python ./evaluation/evaluate_llamaguard.py --language Fr
+python ./evaluation/evaluate_llamaguard.py --language Es
+python ./evaluation/evaluate_llamaguard.py --language De
